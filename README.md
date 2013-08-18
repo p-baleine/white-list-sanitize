9f0efc9c7a3c6a12e578fe46e42b0460cd984f0e
Tajima Junpei <p.baleine@gmail.com>
2013-08-18 19:46:49 +0900

    add object configuration

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # white-list-sanitize [![Build Status](https://travis-ci.org/p-baleine/white-list-sanitize.png?branch=master)](https://travis-ci.org/p-baleine/white-list-sanitize)
 
 Sanitizer based on white list approach.
+
+This library based on darobin's [html-sanitiser](https://github.com/darobin/html-sanitiser) and also provide more controlled sanitize.
+
+## API
+
+Allowed elemtns and attributes can be specified via array.
+
+```js
+var sanitize = require('sanitize');
+
+var opts = {
+  allowedElements: ['a', 'h1'],
+  allowedAttributes: ['href']
+};
+
+sanitize('<a href="//hoge.com">Hoge</a><script></script>', opts, function(err, result) {
+  console.log(result); // => <a href="//hoge.com">Hoge</a>
+});
+```
+
+Also these are specified via object for more controlled sanitize.
+
+```js
+var sanitize = require('sanitize');
+
+var input = [
+  '<div>',
+  '  <iframe src="http://hoge/com"></iframe>',
+  '  <iframe src="//www.youtube.com/embed/N9qV3OvYuJY" frameborder="0" allowfullscreen></iframe>',
+  '</div>'
+].join('');
+
+var opts = {
+  allowedElements: {
+    'div': true,
+    'iframe': {
+      'only': function($elt) {
+        return $elt.attr('src').test(/^¥/¥/www¥.youtube¥.com¥/embed¥/[a-zA-Z0-9]+$/)
+      }
+    }
+  },
+  allowedAttributes: ['src']
+};
+
+sanitize(input, opts, function(err, result) {
+  console.log(result); // => <div><iframe src="//www.youtube.com/embed/N9qV3OvYuJY"></iframe></div>
+});
+```
--- test/white-list-sanitize-test.js
+++ test/white-list-sanitize-test.js
@@ -8,17 +8,51 @@ var expect = require('expect.js'),
 
 describe('white-list-sanitize', function() {
 
-  it('should strip specified tags', function(done) {
-    var input = '<a href="//hoge.com">Hoge</a><h1>Hoge</h1><script></script>',
-        opts = {
-          allowedElements: ['a', 'h1'],
-          allowedAttributes: ['href']
-        };
-    sanitize(input, opts, function(err, res) {
-      expect(err).to.be(null);
-      expect(res).to.equal('<a href="//hoge.com">Hoge</a><h1>Hoge</h1>');
-      done();
+  describe('when opts specified via array', function() {
+
+    it('should strip tags not specified in opts', function(done) {
+      var input = '<a href="//hoge.com">Hoge</a><h1>Hoge</h1><script></script>',
+          opts = {
+            allowedElements: ['a', 'h1'],
+            allowedAttributes: ['href']
+          };
+      sanitize(input, opts, function(err, res) {
+        expect(err).to.be(null);
+        expect(res).to.equal('<a href="//hoge.com">Hoge</a><h1>Hoge</h1>');
+        done();
+      });
     });
+
+  });
+
+  describe('when opts specified via object', function() {
+
+    it('should stript tags which cannot pass specified `only` test', function(done) {
+      var input = [
+        '<div>',
+        '<iframe src="http://hoge/com"></iframe>',
+        '<iframe src="//www.youtube.com/embed/N9qV3OvYuJY"></iframe>',
+        '</div>'
+      ].join('');
+      var opts = {
+        allowedElements: {
+          'div': true,
+          'iframe': {
+            'only': function($elt) {
+              return /^\/\/www\.youtube\.com\/embed\/[a-zA-Z0-9]+$/.test($elt.attr('src'));
+            }
+          }
+        },
+        allowedAttributes: ['src']
+      };
+
+      sanitize(input, opts, function(err, res) {
+        expect(err).to.be(null);
+        expect(res).to.equal('<div><iframe src="//www.youtube.com/embed/N9qV3OvYuJY"></iframe></div>');
+        done();
+      });
+    });
+
   });
 
 });
